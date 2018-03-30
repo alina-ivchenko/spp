81a0b7363dc4fdc4d1cd656663ba0d20cfbc07d5
alina-ivchenko <a_ivch13066@mail.ru>
2018-03-30 07:57:10 +0300

    Faculty & Subject DAO's added

---
+++ test2/src/main/java/Subject.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class Subject {
+    private long idSubject;
+    private String name;
+
+    public long getIdSubject() {
+        return idSubject;
+    }
+
+    public void setIdSubject(long idSubject) {
+        this.idSubject = idSubject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
